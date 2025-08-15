e9278b6f6325ea55cc0152d85b412047fe7a3995
TCC <67762961@qq.com>
2025-08-15 09:46:23 +0800

    fix:标准化更改didt和dvdt的绘图

--- Csv_reader/count_didt.m
+++ Csv_reader/count_didt.m
@@ -59,8 +59,16 @@ end
 
 % 绘图
 % figure;
-Piclength = fix((SWon_stop - SWon_start));
-plot(time(valid_rise_start - Piclength:valid_rise_end + Piclength), ch3(valid_rise_start - Piclength:valid_rise_end + Piclength), 'b');
+SWonlength = fix((SWon_stop - SWon_start));
+
+PicStart = valid_rise_start - SWonlength;
+PicEnd = valid_rise_end + SWonlength;
+PicLength = PicEnd - PicStart;
+PicTop = fix(1.05*max(abs(ch3(PicStart:PicEnd))));
+PicBottom = fix(-0.05*PicTop);
+% PicHeight = PicTop - PicBottom;
+
+plot(time(PicStart:valid_rise_end + SWonlength), ch3(PicStart:valid_rise_end + SWonlength), 'b');
 hold on;
 plot(time(valid_rise_start:valid_rise_end), ch3(valid_rise_start:valid_rise_end), 'r', 'LineWidth',1.5);
 % plot(time(Window_Start:Window_Stop), zeros(Window_Stop-Window_Start+1), 'Black');
@@ -70,13 +78,11 @@ plot(time(valid_rise_end), ch3(valid_rise_end), 'ro', 'MarkerFaceColor','r');
 % 动态标注
 text(time(valid_rise_start+3),ch3(valid_rise_start),['Ic',num2str(didtmode(1)),'=',num2str(ch3(valid_rise_start)),'A'],'FontSize',13);
 text(time(valid_rise_end+3),ch3(valid_rise_end),['Ic',num2str(didtmode(2)),'=',num2str(ch3(valid_rise_end)),'A'],'FontSize',13);
-text(time(valid_rise_start-fix(Piclength*0.9)),max(ch3(SWon_start-50:SWon_stop))*0.9,['Ictop=',num2str(Ictop),'A'],'FontSize',13);
-text(time(valid_rise_start-fix(Piclength*0.9)),max(ch3(SWon_start-50:SWon_stop))*0.8,['di/dt=',num2str(didt),'A/us'],'FontSize',13);
+text(time(PicStart+fix(PicLength*0.05)),PicTop*0.9,['Ictop=',num2str(Ictop),'A'],'FontSize',13);
+text(time(PicStart+fix(PicLength*0.05)),PicTop*0.8,['di/dt=',num2str(didt),'A/us'],'FontSize',13);
 
-% 坐标轴设置
-Hlim = max(ch3(valid_rise_start-Piclength:valid_rise_start+Piclength));
-ylim([-fix(Hlim/20)-1, fix(Hlim/20*21)]);
-xlim([time(valid_rise_start - Piclength), time(valid_rise_end + Piclength)]);
+ylim([PicBottom, PicTop]);
+xlim([time(PicStart), time(PicEnd)]);
 title(['Ic=',num2str(fix(Ictop)),'A di/dt计算']);
 grid on;
 

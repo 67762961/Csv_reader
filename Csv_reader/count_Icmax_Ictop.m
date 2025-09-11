7df199e35ff840558e15858190483a34a48c451d
TCC <67762961@qq.com>
2025-09-11 08:25:07 +0800

    fix:调整Ictop计算

--- Csv_reader/count_Icmax_Ictop.m
+++ Csv_reader/count_Icmax_Ictop.m
@@ -1,11 +1,17 @@
 function [Ictop,tIcm,Icmax] = count_Icmax_Ictop(num,time,ch3,path,dataname,ton1,toff1,cnton1,ton2,toff2)
 
 %% 计算Ictop
+nspd = (time(2)-time(1))*1e9;
 current_interval = ton1 + fix(cnton1/2) : toff1;    % 定义电流峰值搜索区间
 [~, max_idx] = max(ch3(current_interval));          % 快速定位峰值索引 max_idx为相对索引
-tIcm = ton1 + fix(cnton1/2) + max_idx - 1;           % 转换为全局索引
-window_start = max(1, tIcm - 10);                   % 窗口起始：峰值前10点（最小为1）
+tIcm = ton1 + fix(cnton1/2) + max_idx - 1;          % 转换为全局索引
+window_start = max(1, tIcm - fix(30/nspd));        % 窗口起始：峰值前10点（最小为1）
 Ictop = mean(ch3(window_start:tIcm));               % 计算均值
+
+% plot(time(current_interval), ch3(current_interval), 'b');
+% hold on;
+% plot(time(tIcm), ch3(tIcm), 'ro', 'MarkerFaceColor','r');
+% error('1')
 
 %% Icmax 计算
 [Icmax, Icmax_idx] = max(ch3(ton2:toff2));

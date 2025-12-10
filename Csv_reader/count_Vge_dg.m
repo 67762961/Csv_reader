bdb1703f65325991ef98166ef12969dae0bb3770
TCC <67762961@qq.com>
2025-12-10 17:17:41 +0800

    fix:同步修改文件夹路径

--- Csv_reader/count_Vge_dg.m
+++ Csv_reader/count_Vge_dg.m
@@ -48,7 +48,7 @@ title(['Ic=',num2str(fix(Ictop)),'A Vge-dg-on']);
 grid on;
 
 % 路径构建优化
-save_dir = fullfile(path, 'result', dataname, '08 Vge_dg');
+save_dir = fullfile(path, 'result', dataname, '07 Vge_dg');
 if ~exist(save_dir, 'dir'), mkdir(save_dir); end
 saveas(gcf, fullfile(save_dir, [ num, ' Ic=',num2str(fix(Ictop)),'A Vge_dg_on_T',num2str(gd_num),'.png']), 'png');
 close(gcf);

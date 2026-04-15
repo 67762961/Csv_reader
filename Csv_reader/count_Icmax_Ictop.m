c700b148bd8d1a7b9bc0ea4e9c0141c4375f0ddf
TCC <67762961@qq.com>
2026-04-15 17:19:00 +0800

    fix:适配单独电流模式 Ic Id 和负载

--- Csv_reader/count_Icmax_Ictop.m
+++ Csv_reader/count_Icmax_Ictop.m
@@ -8,32 +8,23 @@ toff2=cntVge(cntsw);
 cntoff1 = ton2-toff1;
 
 Id_flag = Ch_labels(5);
+Ic_flag = Ch_labels(3);
 
 static_ic_interval = I_FixBar(1):I_FixBar(2);
 static_id_interval = I_FixBar(3):I_FixBar(4);
 
-%% 计算Ictop
+%%
 nspd = (time(2)-time(1))*1e9;
-
-% 传统计算法Ictop
-current_interval = toff1 : ton2;    % 定义电流峰值搜索区间
-[~, max_idx] = max(ch3(current_interval));          % 快速定位峰值索引 max_idx为相对索引
-tIcm = toff1 + max_idx - 1;          % 转换为全局索引
-window_start = max(1, tIcm - fix(30/nspd));        % 窗口起始：峰值前30ns（最小为1）
-Ictop = mean(ch3(window_start:tIcm));               % 计算均值
-
-% plot(time(current_interval), ch3(current_interval), 'b');
-% hold on;
-% plot(time(tIcm), ch3(tIcm), 'ro', 'MarkerFaceColor','r');
-% error('1')
 
 PicLength = abs(toff2 - ton1);
 PicStart = max(ton1-fix(1*PicLength/4),1);
 PicEnd = min(toff2+fix(2*PicLength/4),length(time));
 PicLength = abs(PicEnd - PicStart);
 Max = max(max(ch3(PicStart:PicEnd)),max(ch5(PicStart:PicEnd)));
+Max = max(Max, max(I_fuzai(PicStart:PicEnd)));
 PicTop = fix(1.1*Max);
 Min = min(min(ch3(PicStart:PicEnd)),min(ch5(PicStart:PicEnd)));
+Min = min(Min, min(I_fuzai(PicStart:PicEnd)));
 PicBottom = fix(min(1.3*Min,-0.1*PicTop));
 PicHeight = PicTop - PicBottom;
 
@@ -41,6 +32,26 @@ barheight = 0.02*PicHeight;
 
 close all;
 figure('Position', [320, 240, 1600/DPI, 600/DPI]);
+
+if Ic_flag~=0
+    % 传统计算法Ictop
+    current_interval = toff1 : ton2;    % 定义电流峰值搜索区间
+    [~, max_idx] = max(ch3(current_interval));          % 快速定位峰值索引 max_idx为相对索引
+    tIcm = toff1 + max_idx - 1;          % 转换为全局索引
+    window_start = max(1, tIcm - fix(30/nspd));        % 窗口起始：峰值前30ns（最小为1）
+    Ictop = mean(ch3(window_start:tIcm));               % 计算均值
+    % Ic校准线及标注
+    barStart = static_ic_interval(1);
+    barEnd = static_ic_interval(end);
+    line([time(barStart),time(barEnd)],[0,0],'Color', [0.5 0.5 0.5],'LineStyle','--');
+    line([time(barStart),time(barStart)],[0-barheight, 0+barheight], 'Color', [0.5 0.5 0.5]);
+    line([time(barEnd),time(barEnd)],[0-barheight, 0+barheight], 'Color', [0.5 0.5 0.5]);
+    % Ic绘图
+    plot(time(PicStart:PicEnd), ch3(PicStart:PicEnd), 'Color','r');
+    hold on;
+    plot(time(tIcm), Ictop, 'ro', 'MarkerFaceColor','r');
+    text(time(tIcm),Ictop + fix(PicHeight*0.05),['Ictop =',num2str(Ictop),'A'], 'FontSize',13,'Color','r');
+end
 
 % 若有Id输入 则以静态区Id值作为Ictop
 if Id_flag~=0
@@ -87,31 +98,6 @@ if Fuzaimode~=0
     plot(time(PicStart:PicEnd), I_fuzai(PicStart:PicEnd), 'Color','g');
 end
 
-% Ic校准线及标注
-barStart = static_ic_interval(1);
-barEnd = static_ic_interval(end);
-line([time(barStart),time(barEnd)],[0,0],'Color', [0.5 0.5 0.5],'LineStyle','--');
-line([time(barStart),time(barStart)],[0-barheight, 0+barheight], 'Color', [0.5 0.5 0.5]);
-line([time(barEnd),time(barEnd)],[0-barheight, 0+barheight], 'Color', [0.5 0.5 0.5]);
-% Ic绘图
-plot(time(PicStart:PicEnd), ch3(PicStart:PicEnd), 'Color','r');
-hold on;
-plot(time(tIcm), Ictop, 'ro', 'MarkerFaceColor','r');
-text(time(tIcm),Ictop + fix(PicHeight*0.05),['Ictop =',num2str(Ictop),'A'], 'FontSize',13,'Color','r');
-
-%% Icmax 计算
-[Icmax, Icmax_idx] = max(ch3(ton2:toff2));
-Icmax_idx = ton2 + Icmax_idx - 1;
-
-%% 电流采信选择
-if Id_flag==0
-    I_meature = "Ic";
-end
-
-if Ch_labels(3) ==0
-    Ictop = FuzaiTop;
-end
-
 if (Fuzaimode ~= 0) && (I_meature == "I_fuzai")
     I_Fuizai_on  = mean(I_fuzai(ton2-fix(30/nspd):ton2+fix(30/nspd)));
     I_Fuizai_off = mean(I_fuzai(toff1-fix(30/nspd):toff1+fix(30/nspd)));
@@ -119,30 +105,52 @@ if (Fuzaimode ~= 0) && (I_meature == "I_fuzai")
     text(time(ton2),I_Fuizai_on - fix(PicHeight*0.1),['I_on =',num2str(I_Fuizai_on),'A'], 'FontSize',10,'Color','g');
     plot(time(toff1), I_Fuizai_off, 'go');
     text(time(toff1),I_Fuizai_off - fix(PicHeight*0.1),['I_off =',num2str(I_Fuizai_off),'A'], 'FontSize',10,'Color','g');
-else
-    I_Fuizai_on = Ictop;
-    I_Fuizai_off = Ictop;
+end
+
+%% 电流采信选择
+for i = 1:4
+    if Id_flag==0
+        I_meature = "Ic";
+        
+    elseif Ic_flag ==0
+        I_meature = "I_fuzai";
+        
+    elseif Fuzaimode == 0
+        I_meature = "Id";
+    end
+    % disp(['I_meature = ', I_meature]);
 end
 
 switch I_meature
     case "Ic"
         Ictop_out = Ictop;
+        I_Fuizai_on = Ictop_out;
+        I_Fuizai_off = Ictop_out;
     case "Id"
         Ictop_out = -1*Idbase;
+        I_Fuizai_on = Ictop_out;
+        I_Fuizai_off = Ictop_out;
     case "I_fuzai"
         Ictop_out = FuzaiTop;
     otherwise
         error('I_meature参数错误 请检查');
 end
 
-% 绘图
-plot(time(Icmax_idx), Icmax, 'ro', 'MarkerFaceColor','r');
-text(time(Icmax_idx+fix(PicLength*0.05)),Icmax,['Icmax=',num2str(Icmax),'A'], 'FontSize',13,'Color','r');
+%% Icmax 计算
+if Ic_flag
+    [Icmax, Icmax_idx] = max(ch3(ton2:toff2));
+    Icmax_idx = ton2 + Icmax_idx - 1;
+    % 绘图
+    plot(time(Icmax_idx), Icmax, 'ro', 'MarkerFaceColor','r');
+    text(time(Icmax_idx+fix(PicLength*0.05)),Icmax,['Icmax=',num2str(Icmax),'A'], 'FontSize',13,'Color','r');
+else
+    Icmax = 0;
+end
+
 ylim([PicBottom, PicTop]);
 xlim([time(PicStart), time(PicEnd)]);
 title(['Ic=',num2str(fix(Ictop_out)),' A Icmax']);
 grid on;
-
 save_dir = fullfile(path, 'result', dataname, '01 Icmax & Ictop');
 if ~exist(save_dir, 'dir'), mkdir(save_dir); end
 saveas(gcf, fullfile(save_dir, [ num,' Ic=',num2str(fix(Ictop_out)),'A Icmax.png']), 'png');
